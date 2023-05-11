c0cb9bf8fa971c048f9b6dfd046f0bbe14a5235f
alahdili <aymanlahdili25@gmail.com>
2023-05-11 19:15:36 -0400

    Completed generateGrid function

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+function generateGrid(dimension) {
+  container.style.setProperty('--grid-rows', dimension);
+  container.style.setProperty('--grid-cols', dimension);
+  for (c = 0; c < (dimension * dimension); c++) {
     let cell = document.createElement("div");
     // cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(10, 10);
+generateGrid(24);
