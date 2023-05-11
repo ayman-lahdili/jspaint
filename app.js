080b75d9158eb8f03c01087324a702916a24023e
alahdili <aymanlahdili25@gmail.com>
2023-05-11 19:33:03 -0400

    Added click to draw a pixel in black

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const container = document.getElementById("container");
 
 function generateGrid(dimension) {
-  container.style.setProperty('--grid-rows', dimension);
-  container.style.setProperty('--grid-cols', dimension);
-  for (c = 0; c < (dimension * dimension); c++) {
-    let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
+    container.style.setProperty('--grid-rows', dimension);
+    container.style.setProperty('--grid-cols', dimension);
+    // for (c = 0; c < (dimension * dimension); c++) {
+    //     let cell = document.createElement("div");
+    //     // cell.innerText = (c + 1);
+    //     container.appendChild(cell).className = "grid-item";
+    // };
+
+    for (i = 0; i <dimension; i++) {
+        for (j = 0; j<dimension; j++) {
+            let cell = document.createElement("div");
+
+            cell.className = "grid-item";
+            container.appendChild(cell).id  = `${i}-${j}`;
+
+            cell.addEventListener("click", () => {
+                cell.style.cssText = "background-color: black;"
+            })
+        }
+    }
 };
 
 generateGrid(24);
