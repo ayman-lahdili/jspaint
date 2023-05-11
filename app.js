bb7a9b6346c28a8614ec36125eed237237a9079f
alahdili <aymanlahdili25@gmail.com>
2023-05-11 19:35:01 -0400

    Formatted

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const container = document.getElementById("container");
 function generateGrid(dimension) {
     container.style.setProperty('--grid-rows', dimension);
     container.style.setProperty('--grid-cols', dimension);
-    // for (c = 0; c < (dimension * dimension); c++) {
-    //     let cell = document.createElement("div");
-    //     // cell.innerText = (c + 1);
-    //     container.appendChild(cell).className = "grid-item";
-    // };
 
     for (i = 0; i <dimension; i++) {
         for (j = 0; j<dimension; j++) {
